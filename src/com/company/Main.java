78e857db1565109e70d899040b2522268a381a3b
sepehrMSP <sepehrpourghannad@yahoo.com>
2019-03-07 09:54:38 +0330

    NEW FOR ADDED

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,5 +3,7 @@ package com.company;
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello Ali");
+        for (int i=0 ; i < 79 ;i++)
+            System.out.println("PUUUUUUUUUUUUUUUUUUUUUUUUUUUL");
     }
 }
