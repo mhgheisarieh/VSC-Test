7a3f32e9b4dc0a8fdcb319aeb3c910170617c905
sepehrMSP <sepehrpourghannad@yahoo.com>
2019-03-07 11:34:46 +0330

    a garbage branch

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,9 +2,10 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello Ali");
+        System.out.println("hello Hasan");
         for (int i=0 ; i < 79 ;i++)
             System.out.println("Helloooo");
-
+        System.out.println("Goood");
+        System.out.println("badaab");   //buuuuuuuuuuuuuuuuug
     }
 }
